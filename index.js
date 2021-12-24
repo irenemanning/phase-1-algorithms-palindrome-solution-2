2c213bc41f39cd550c795eb6c8c0e71c71d578e6
Irene <irenemanningm@gmail.com>
2021-12-23 23:11:31 -0600

    commit tree d7e7458cf2d4cd8f9d484231f87b54df749da9bd

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  word.length - 1/2
+  for (let i=0; i < word.length / 2; i++) {
+    const j = word.length - 1 - i
+    
+    if (word[i] !== word[j]) return false;
+  }
+  return true
 }
 
 /* 
   Add your pseudocode here
+  compare the 
 */
 
 /*
   Add written explanation of your solution here
+  in a string, if the last letter is the the same as the first letter, and seccond to last letter is same as the seccond letter and so on untill a middle is reached, 
+  return true
+  if any letter doesn't match, return false
 */
 
 // You can run `node index.js` to view these console logs
